1963b1d7ab9e05579535ab293a2ff8c4deaec686
harjup <pikmin@gmail.com>
2016-05-19 21:56:04 -0400

    Add basic scripting commands for a horse character, add to the yarn script
    - can walk to different sides of the screen
    - can vibrate intensely!
    - can change which horse they are!

---
+++ src/HorseDate/Assets/src/Scripts/Horse.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using DialoguerCore;
+using DialoguerEditor;
+using UnityEngine.UI;
+using Yarn.Unity;
+
+public class Horse : MonoBehaviour 
+{
+    // walk center|left
+    // switch brown|striped|bancho
+    // vibrate
+
+    private Sprite BrownSprite;
+    private Sprite StripedSprite;
+    private Sprite BanchoSprite;
+
+    private Image Image;
+
+    public void Start()
+    {
+        Image = GetComponent<Image>();
+
+        BrownSprite = Resources.Load<Sprite>("Images/brown-horse-transparent");
+        StripedSprite = Resources.Load<Sprite>("Images/striped-horse");
+        BanchoSprite = Resources.Load<Sprite>("Images/bancho-horse");
+    }
+
+    [YarnCommand("walk")]
+    public void Walk(string position)
+    {
+        if (position == "center")
+        {
+            transform.DOLocalMove(new Vector3(-200, -90, 0), 1f).SetEase(Ease.OutExpo);
+        }
+
+        if (position == "left")
+        {
+            transform.DOLocalMove(new Vector3(-645, -90, 0), 1f).SetEase(Ease.OutExpo);
+        }
+    }
+
+    [YarnCommand("switch")]
+    public void Switch(string skin)
+    {
+        switch (skin.ToLower())
+        {
+            case "brown":
+                Image.sprite = BrownSprite;
+                break;
+            case "striped":
+                Image.sprite = StripedSprite;
+                break;
+            case "bancho":
+                Image.sprite = BanchoSprite;
+                break;
+        }
+    }
+
+    [YarnCommand("vibrate")]
+    public void Vibrate()
+    {
+        transform.DOShakePosition(1f, Vector2.one * 5f).SetLoops(-1);
+    }
+}
