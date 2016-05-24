5444cb0f85beefbb3e03e45fc4a7292eeb897f57
harjup <pikmin@gmail.com>
2016-05-23 20:31:17 -0400

    Add dialog for field scene, add Dialog script for starting nodes from Yarn

---
+++ src/HorseDate/Assets/src/Scripts/Dialog.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq.Expressions;
+using Yarn.Unity;
+
+public class Dialog : MonoBehaviour
+{
+    public bool IsApple;
+
+    private DialogueRunner _runner;
+
+    private DialogueRunner Runner
+    {
+        get { return _runner ?? (_runner = FindObjectOfType<DialogueRunner>()); }
+    }
+
+    void Start()
+    {
+        var introScript = Resources.Load<TextAsset>("Text/Intro");
+        Runner.AddScript(introScript);
+        Runner.StartDialogue("Start");
+    }
+
+    [YarnCommand("run")]
+
+    public void Run(string startNode)
+    {
+        if (startNode.ToLower() == "field")
+        {
+            Runner.StartDialogue("Field");
+        }
+    }
+}
